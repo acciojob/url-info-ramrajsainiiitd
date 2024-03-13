4ee5b9d5724c69eb082609e1aa86bac54ef42b41
ramrajsainiiitd <109168257+ramrajsainiiitd@users.noreply.github.com>
2024-03-13 10:40:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+const currentUrl = window.location.href;
+lengthOfCurrURL = currentUrl.length;
+//console.log(currentUrl,lengthOfCurrURL);
+alert(`The length of the URL is: ${lengthOfCurrURL}`);
